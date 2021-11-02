3083c787a9b6614ca20c435b5b4dbfd4e548bf18
Abedrahim Ben rebia <b.abedrahim@tomba.io>
2021-11-02 08:00:02 +0000

    version 1.0.1

---
+++ R/tomba-package.R
@@ -0,0 +1,11 @@
+#' @title The official R client library for the tomba.io
+#' @description Search emails are based on the website You give one domain name and it returns all the email addresses found on the internet. at <https://tomba.io/>
+#'
+#' @md
+#' @name tomba
+#' @docType package
+#' @author Abedrahim Ben rebia <b.abedrahim@tomba.io>
+#' @import httr
+#' @import jsonlite
+#' @import methods
+NULL
